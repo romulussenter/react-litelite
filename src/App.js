f61b8d27cf0aaed3ca35fcbbcf35e41d462e6903
Romulus <romulus.senter@yahoo.com>
2018-02-16 19:36:01 -0500

    Finshed up on my litelite application. tested and my reset button works.

--- src/App.js
+++ src/App.js
@@ -1,66 +1,312 @@
-import React, { Component } from 'react';
-import './App.css';
-import './LiteGrid';
-import LiteGrid from './LiteGrid';
+import React, { Component } from "react";
+import "./App.css";
+import "./LiteGrid";
+import LiteGrid from "./LiteGrid";
 
 class App extends Component {
-  constructor(){
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      cards: [{
-            bg:'blue '
-      },
-      {
-        bg:'blue'
-  },
-  {
-    bg:'blue'
-},
-{
-  bg:'blue'
-},
-{
-  bg:'blue'
-},
-{
-  bg:'blue'
-},
-{
-  bg:'blue'
-}
+      cards: [
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        },
+        {
+          bg: "blue"
+        }
       
-  ]
+      ]
     }
+  
+    this.changeColor = this.changeColor.bind(this);
+    this.reset = this.reset.bind(this);
+  
+}
+
+
+  
+      
+
+  changeColor(position) {
+    const currentBox = this.state.cards[position];
+    const first = this.state.cards.slice(0, position);
+    const last = this.state.cards.slice(position + 1);
+    const newBox = {
+      ...currentBox,
+      bg: "red"
+    }
+    const newBoxes = [...first, newBox, ...last];
+    this.setState({ cards: newBoxes });
   }
   
-  render() {
+  reset(){
+    const newCards = this.state.cards.map(card => ({bg:'blue'}))
+    console.log(newCards);
+    this.setState({
+      cards: newCards
+    });
+  }
+          
   
+
+  render() {
+    const cards = this.state.cards.map((card, index) => (
+      <div
+        key={index}
+        style={{ backgroundColor: card.bg }}
+        className="col-md-1 cardy"
+        onClick={() => this.changeColor(index)}
+      />
+    ));
+
     return (
       <div className="container">
-      <h1>Lite Lite</h1>
-      <div className='row'>
-        {this.state.cards.map((cardObj, index) => <div style={{backgroundColor: cardObj.bg}} 
-      key={index} className="col-md-1 border"></div>)}                                                   
-   </div>
-   </div>
-   
-   
+        <h1>Lite Lite</h1>
+        <div className="row">{cards}</div>
+        <button className="col-md-12 btn btn-primary" onClick={this.reset} >Clear Grid</button>
+          
+        
+      </div>
+        
       
     );
   }
+}
 
-  
-    
-    
-    
-
-     
-} 
-    
-   
-    
-    
-    
 export default App;
 // {/* return (
 //       <div className='container'>
@@ -94,8 +340,8 @@ export default App;
 //        <div className='col-md-1 border'></div>
 //       </div>
 //       </div>
-      
+
 //     );
-  
+
 // {/* < LiteGrid /> */}
 // } */}
